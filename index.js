efc4244818b9c39c9b51e5cb5ba1ded4274092e6
Alex <alex.druk@gmail.com>
2018-05-14 16:27:34 +0200

    works with BB

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ const interval = args[4].toString();//
 const filename = './data/' + instrument + '_' + TIME.today().replace(' 00:00','')+ '_' + interval + '.json';
 const datafile = './data/results_' + instrument + '_' + TIME.today().replace(' 00:00','')+ '_' + interval + '.json';
 console.log(platform, instrument, interval, filename);
-const MFIperiods = [20,21,22,23,24,25,26,27,28,29];
+const MFIperiods = [14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+const MFIlags = [1.0, 1.5, 2.0];
 
 //let low, high, open, close, vol, at = [];
 let ins = {
@@ -55,26 +56,42 @@ async function main() {
     else {
         ins = await chromium.main(platform, instrument, interval, filename);
     }
-
-//deal with data
+/*
+    let bbResults = await talib.bb(ins.close, 1, 14,  2, 2, 0);
+    let bbUpperBand = bbResults.outRealUpperBand;
+    let bbLowerBand = bbResults.outRealLowerBand;
+    let bbMiddleBand = bbResults.outRealMiddleBand;
+    console.log(bbUpperBand.pop(), bbLowerBand.pop(), bbMiddleBand.pop());
+    console.log('end')
+    process.exit(0);
+*/
+    //deal with data
     let MFIrange = {};
     for (const period of MFIperiods) {
         trading.storageIni(storage);
-        for (let i = 100; i < ins.at.length; i++) { //100 tom leave some buffer like 500 in CT
+        for (let i = 100; i < ins.at.length; i++) { //100 to leave some buffer like 500 in CT
             let high = ins.high.slice(0, i);
             let open = ins.open.slice(0, i);
             let low = ins.low.slice(0, i);
             let close = ins.close.slice(0, i);
             let vol = ins.volume.slice(0, i);
-            let mfiResults = await talib.mfi(high, low, close, vol, 1, period);
-            trading.mfi(close.pop(), mfiResults.pop(), storage);
+//            let mfiResults = await talib.mfi(high, low, close, vol, 1, period);
+//            trading.mfi(close.pop(), mfiResults.pop(), storage);
+            let std = await talib.std (close, 1, period);
+            let bbResults = await talib.bb(close, 1, period,  2, 2, 0);
+            let bbUpperBand = bbResults.outRealUpperBand;
+            let bbLowerBand = bbResults.outRealLowerBand;
+            let bbMiddleBand = bbResults.outRealMiddleBand;
+            trading.bb(close.pop(), bbUpperBand.pop(), bbLowerBand.pop(), std.pop(), storage);
         }
         MFIrange[period] = storage.pl;
+        console.log(period, storage.pl);
     }
-    let res = Object.keys(MFIrange).reduce((a, b) => MFIrange[a] > MFIrange[b] ? a : b);
-    console.log(res, MFIrange[res]);
+    let MFIres = Object.keys(MFIrange).reduce((a, b) => MFIrange[a] > MFIrange[b] ? a : b);
+    const MFI_optimum_period = MFIres;
+    console.log(MFIres, MFIrange[MFIres]);
 
-    /*
+/*
         try {
             fs.appendFileSync(datafile, result);
         } catch (err) {
