c51b7e957f6575682e0e043f39f76a2e5f05ddb8
Alex <alex.druk@gmail.com>
2018-07-31 16:41:10 +0200

    end process if no DB connection

--- db_amazon.js
+++ db_amazon.js
@@ -14,12 +14,15 @@ pool.getConnection((err, connection) => {
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
             console.error('Database connection was closed.')
         }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
+        else if (err.code === 'ER_CON_COUNT_ERROR') {
             console.error('Database has too many connections.')
         }
-        if (err.code === 'ECONNREFUSED') {
+        else if (err.code === 'ECONNREFUSED') {
             console.error('Database connection was refused.')
         }
+        else 
+            throw err;
+            process.exit(1); 
     }
 
     if (connection) connection.release()
