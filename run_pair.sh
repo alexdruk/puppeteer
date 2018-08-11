25a68238b9fc415a35536be8b96dee91ff269b07
Alex <alex.druk@gmail.com>
2018-08-11 19:04:59 +0200

    shell script for cron

---
+++ run_pair.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if [ $# -ne 2 ]
+  then
+    echo "No or wrong arguments supplied. Exiting..."
+    exit 1
+fi
+now=$(date +"%T")
+date=$(date +'%m-%d')
+echo "Starting $2 1m : $now"
+node index.js $1 $2 1m >> "./res/$1_$2_1m_$date.txt"
+now=$(date +"%T")
+echo "Starting $2 5m : $now"
+node index.js $1 $2 5m >> "./res/$1_$2_5m_$date.txt"
+now=$(date +"%T")
+echo "Starting $2 15m : $now"
+node index.js $1 $2 15m >> "./res/$1_$2_15m_$date.txt"
+now=$(date +"%T")
+echo "Starting $2 30m : $now"
+node index.js $1 $2 30m >> "./res/$1_$2_30m_$date.txt"
+now=$(date +"%T")
+echo "Starting $2 1h : $now"
+node index.js $1 $2 1h >> "./res/$1_$2_1h_$date.txt"
+now=$(date +"%T")
+echo "Starting $2 2h : $now"
+node index.js $1 $2 2h >> "./res/$1_$2_2h_$date.txt"
+now=$(date +"%T")
+echo "End of all : $now"
