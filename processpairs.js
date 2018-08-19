b611174bb2a8919305d0eaaab96318157f59617c
Alex <alex.druk@gmail.com>
2018-08-19 09:02:28 +0200

    add logging at the beginning

--- processpairs.js
+++ processpairs.js
@@ -10,11 +10,19 @@ let cpuCount = os.cpuCount()-2;
 async function main() {
     try {
         let cpuUsage5min = os.loadavg(5); //%
+            console.log('cpuCount=', cpuCount, ' cpuUsage5min=', cpuUsage5min);
         if (cpuUsage5min > 15) {
             console.log('CPU usage above the treshold', cpuUsage5min);
             await f.sleep(600000);//sleep 10 min in cpu load > 15%
         }
         let result = await f.getPairs(cpuCount).catch(e => {console.log(e);process.exit(1);});
+        if (result.length > 0) {
+            console.log(result.length+1,  ' pairs will be updated.');
+        }
+        else {
+            console.log('No pairs selected. Exiting...');
+            process.exit(1);
+        }
         let  data = JSON.parse(result);
         let count = 0;
         data.forEach(async el => {
@@ -26,7 +34,6 @@ async function main() {
                 }
                 let pair = el.pair_name;
                 let market = el.m_name;
-                let today = new Date();
                 let child = spawn(path+'run_pair.sh',[market, pair], {
                     shell:true,
                     detached: true,
@@ -36,9 +43,7 @@ async function main() {
                     console.log('Err: ' + err);
                   });
                 console.log('Child process run_pair.sh with ', market, pair, ' started with pid ', child.pid);
-                let sqlResult = await f.set_in_work(market, pair).catch(e => {console.log(e);});
-                if (sqlResult) {console.log('in_work was updated');}
-                await f.sleep(120000);//sleep 2 min between intervals
+                 await f.sleep(120000);//sleep 2 min between intervals
         });
     }
     catch(e) {console.log(e); process.exit(1);} 
