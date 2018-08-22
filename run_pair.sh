12515253754f50a372d6ae77f4a64e3bb18de5b7
Alex <alex.druk@gmail.com>
2018-08-22 18:43:32 +0200

    rewrote to loop of arr of intervals

--- run_pair.sh
+++ run_pair.sh
@@ -4,21 +4,14 @@ if [ $# -ne 2 ]
     echo "No or wrong arguments supplied. Exiting..."
     exit 1
 fi
-now=$(date +"%T")
+declare -a arr=("5m" "15m" "30m" "1h" "2h")
 date=$(date +'%m-%d')
-echo "Starting $2 5m : $now"
-node index.js $1 $2 5m >> "./res/$1_$2_5m_$date.txt"
+
+for i in "${arr[@]}"
+do
+	now=$(date +"%T")
+	echo "Starting $2 $i : $now"
+	node index.js $1 $2 $i >> "/home/ec2-user/puppeteer/res/$1_$2_$i_$date.txt"
+done
 now=$(date +"%T")
-echo "Starting $2 15m : $now"
-node index.js $1 $2 15m >> "./res/$1_$2_15m_$date.txt"
-now=$(date +"%T")
-echo "Starting $2 30m : $now"
-node index.js $1 $2 30m >> "./res/$1_$2_30m_$date.txt"
-now=$(date +"%T")
-echo "Starting $2 1h : $now"
-node index.js $1 $2 1h >> "./res/$1_$2_1h_$date.txt"
-now=$(date +"%T")
-echo "Starting $2 2h : $now"
-node index.js $1 $2 2h >> "./res/$1_$2_2h_$date.txt"
-now=$(date +"%T")
-echo "End of all : $now"
+echo "End of all $2: $now"
