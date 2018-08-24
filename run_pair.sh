9cdd45bd70c347b8a024c0b2285796dcb7f92764
Alex <alex.druk@gmail.com>
2018-08-24 11:56:08 +0200

    solved interal problem is log files

--- run_pair.sh
+++ run_pair.sh
@@ -4,14 +4,14 @@ if [ $# -ne 2 ]
     echo "No or wrong arguments supplied. Exiting..."
     exit 1
 fi
-declare -a arr=("5m" "15m" "30m" "1h" "2h")
+declare -a arr=(5m 15m 30m 1h 2h)
 date=$(date +'%m-%d')
 
 for i in "${arr[@]}"
 do
 	now=$(date +"%T")
 	echo "Starting $2 $i : $now"
-	node index.js $1 $2 $i >> "/home/ec2-user/puppeteer/res/$1_$2_$i_$date.txt"
+	/home/ec2-user/.nvm/versions/node/v8.11.2/bin/node /home/ec2-user/puppeteer/index.js $1 $2 $i >> "/home/ec2-user/puppeteer/res/$1_$2_${i}_$date.txt"
 done
 now=$(date +"%T")
 echo "End of all $2: $now"
