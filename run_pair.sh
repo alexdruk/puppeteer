0026b3a551eb56d8023a2b9c7cd794d3f6279ab1
Alex <alex.druk@gmail.com>
2018-08-27 16:41:28 +0200

    add 1m interval

--- run_pair.sh
+++ run_pair.sh
@@ -4,7 +4,7 @@ if [ $# -ne 2 ]
     echo "No or wrong arguments supplied. Exiting..."
     exit 1
 fi
-declare -a arr=(5m 15m 30m 1h 2h)
+declare -a arr=(1m 5m 15m 30m 1h 2h)
 date=$(date +"%m-%d")
 
 for i in "${arr[@]}"
