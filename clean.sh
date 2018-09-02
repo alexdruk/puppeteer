4df5394f9a4e99a140b79d892a13e320b3b4707a
Alex <alex.druk@gmail.com>
2018-09-02 11:15:49 +0200

    added messages for logging

--- clean.sh
+++ clean.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+find /var/log/cron*  -type f -mtime +5
 find /var/log/cron*  -type f -mtime +5 -exec rm {} +
+find /var/log/messages*  -type f -mtime +5
 find /var/log/messages*  -type f -mtime +5 -exec rm {} +
+find /var/log/httpd/access_log*  -type f -mtime +
 find /var/log/httpd/access_log*  -type f -mtime +5 -exec rm {} +
+find /var/log/httpd/error_log*  -type f -mtime +10
 find /var/log/httpd/error_log*  -type f -mtime +10 -exec rm {} +
+find /home/ec2-user/puppeteer/data/*  -type f -mtime +4
 find /home/ec2-user/puppeteer/data/*  -type f -mtime +4 -exec rm {} +
+find /home/ec2-user/puppeteer/res/*  -type f -mtime +4
 find /home/ec2-user/puppeteer/res/*  -type f -mtime +4 -exec rm {} +
